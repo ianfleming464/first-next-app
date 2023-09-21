44e53d2039c736b2cdd3b838383ed19c8ccbf897
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-09-21 21:48:22 +0200

    Individual Todo component now working with RTK as opposed to Context

--- app/todos/[id]/page.js
+++ app/todos/[id]/page.js
@@ -1,17 +1,12 @@
 'use client';
-import { useApi } from '../../api';
-import { useContext } from 'react';
-import { TodoContext } from '../../store/todo-context';
+import { useSelector } from 'react-redux';
 
 export default function ToDo({ params }) {
-  const { todos, updateTodo } = useContext(TodoContext);
-  const currentTodo = todos.filter(todo => todo.id === Number(params.id))[0]; // turns out params.id is a string. This changes the type to a number. Then, because filter returns an array, we grab the first and only item in the array.
-  console.log(currentTodo, typeof currentTodo);
-  // console.log('params.id data type:', typeof params.id);
+  const todos = useSelector(state => state.todo.todos);
+
+  const currentTodo = todos.filter(todo => todo.id === Number(params.id))[0];
 
   if (currentTodo) {
-    // console.log('Todo ID:', currentTodo.id);
-    // console.log('Todo Title:', currentTodo.title);
     return (
       <main className='flex min-h-screen flex-col items-center p-24'>
         <h3 className='text-2xl font-bold text-center mb-6'>To-Do Number : {currentTodo.id} </h3>
